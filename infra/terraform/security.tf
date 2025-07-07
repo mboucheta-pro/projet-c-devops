40816d209bb664ac444706a936de42183f03290a
Mohamed Boucheta <mohamed.boucheta@sncf.fr>
2025-07-07 19:15:10 +0200

    update security.tf

--- infra/terraform/security.tf
+++ infra/terraform/security.tf
@@ -20,21 +20,6 @@ resource "aws_security_group" "infra_instances" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # GitLab HTTP/HTTPS
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   # SonarQube
   ingress {
     from_port   = 9000
@@ -50,8 +35,6 @@ resource "aws_security_group" "infra_instances" {
     protocol    = "tcp"
     cidr_blocks = [var.vpc_infra_cidr]
   }
-
-
 
   egress {
     from_port   = 0
