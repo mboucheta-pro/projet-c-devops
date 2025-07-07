60f624c6a29a8092d2c960cd5a6cea8f2dae3667
Mohamed Boucheta <mohamed.boucheta@sncf.fr>
2025-07-07 15:11:17 +0200

    update infrastructure.yml and main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "projet-c-terraform-state-devops"
+    bucket         = "projet-c"
     key            = "devops/terraform.tfstate"
     region         = "ca-central-1"
     encrypt        = true
