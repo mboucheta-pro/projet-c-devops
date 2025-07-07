a384f57cbac33a75ffd0de5f5e38b7f972ab06c8
Mohamed Boucheta <mohamed.boucheta@sncf.fr>
2025-07-07 15:34:34 +0200

    update variables.tf and vpc.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -2,40 +2,36 @@
 variable "project" {
   description = "Nom du projet"
   type        = string
+  default = "projet-c-mohamed"
 }
 
 variable "region" {
   description = "Région AWS"
   type        = string
+  default = "ca-central-1"
 }
 
 # Configuration réseau
 variable "vpc_infra_cidr" {
   description = "CIDR du VPC Infrastructure CI/CD"
   type        = string
-}
-
-variable "vpc_app_cidr" {
-  description = "CIDR block for the application VPC to allow routing from infra VPC"
-  type        = string
-}
-
-variable "vpc_app_id" {
-  description = "ID de la VPC applicative"
-  type        = string
+  default = "10.1.0.0/16"
 }
 
 variable "ubuntu_ami" {
   description = "AMI Ubuntu 24.04 LTS"
   type        = string
+  default = "ami-0083d3f8b2a6c7a81"  # Ubuntu 24.04 LTS ca-central-1
 }
 
 variable "tf_backend_bucket" {
   description = "Nom du bucket S3 pour le backend Terraform"
   type        = string
+  default = var.project
 }
 
 variable "tf_backend_dynamodb" {
   description = "Nom de la table DynamoDB pour les verrous Terraform"
   type        = string
+  default = "terraform-locks"
 }
--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -42,12 +42,6 @@ resource "aws_route_table" "infra_public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.infra.id
   }
-
-  route {
-    cidr_block                = var.vpc_app_cidr
-    vpc_peering_connection_id = aws_vpc_peering_connection.infra_to_app.id
-  }
-
   tags = {
     Name = "vpc-infra-public-rt"
   }
@@ -58,10 +52,4 @@ resource "aws_route_table_association" "infra_public" {
   count          = 2
   subnet_id      = aws_subnet.infra_public[count.index].id
   route_table_id = aws_route_table.infra_public.id
-}
-
-resource "aws_vpc_peering_connection" "infra_to_app" {
-  vpc_id      = aws_vpc.infra.id
-  peer_vpc_id = var.vpc_app_id
-  auto_accept = true
 }
