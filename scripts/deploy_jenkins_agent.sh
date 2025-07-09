c3dab87743861c4bad73b4ccf88285731c577f59
Mohamed Boucheta <mohamed.boucheta@sncf.fr>
2025-07-09 22:28:35 +0200

    update jenkins-agent-playbook.yml and deploy_jenkins_agent.sh

--- scripts/deploy_jenkins_agent.sh
+++ scripts/deploy_jenkins_agent.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 echo "🚀 Déploiement Jenkins Agent simplifié..."
 
 # Récupérer les credentials depuis AWS Secrets Manager
+export JENKINS_ADMIN_PASSWORD=$(aws secretsmanager get-secret-value --secret-id projet-c-devops-jenkins-credentials --query SecretString --output text --region ca-central-1 | jq -r '.admin_password')
+export JENKINS_ADMIN_USERNAME=$(aws secretsmanager get-secret-value --secret-id projet-c-devops-jenkins-credentials --query SecretString --output text --region ca-central-1 | jq -r '.admin_username')
 SSH_PRIVATE_KEY=$(aws secretsmanager get-secret-value --secret-id SSH_PRIVATE_KEY --query SecretString --output text --region ca-central-1)
 
 # Créer la clé SSH temporaire
@@ -17,12 +19,13 @@ cd $GITHUB_WORKSPACE/infra/ansible
 # Créer l'inventaire simple
 cat > inventory.ini << EOF
 [jenkins_agents]
-jenkins-agent ansible_host=${JENKINS_AGENT_IP} ansible_user=ubuntu ansible_ssh_private_key_file=${SSH_KEY_FILE} jenkins_master_ip=${JENKINS_IP}
+jenkins-agent ansible_host=${JENKINS_AGENT_IP} ansible_user=ubuntu ansible_ssh_private_key_file=${SSH_KEY_FILE}
 EOF
 
 # Déployer avec le playbook simplifié
 ansible-playbook -i inventory.ini jenkins-agent-playbook.yml -v
+
 # Nettoyer
 rm -f inventory.ini "$SSH_KEY_FILE"
 
-echo "✅ Déploiement terminé"
+echo "✅ Déploiement Jenkins Agent terminé"
